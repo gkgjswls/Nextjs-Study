c202e79ff68e66db91f95047b6e92874616b0614
gkgjswls <gkgjswls842@gmail.com>
2023-05-05 20:18:46 +0900

    사전데이터 패칭

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import EventList from '../components/events/eventList'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-import { getFeaturedEvents } from '../data/dummy-data'
-
-export default function HomePage() {
+import { getFeaturedEvents } from '../helpers/api-util'
+export default function HomePage(props) {
 
   const featuredEvednts = getFeaturedEvents()
   return (
     <div>
-      <EventList items={featuredEvednts}/>
+      <EventList items={props.events}/>
     </div>
   )
+}
+export async function getStaticProps(context){
+  const featuredEvednts = await getFeaturedEvents();
+
+  return {
+    props: {
+      events:  featuredEvednts
+    }
+  }
 }
