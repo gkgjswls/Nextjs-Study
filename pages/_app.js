a01cc7ee75e07360bb9e7b60685cf6bc974ab64e
gkgjswls <gkgjswls842@gmail.com>
2023-05-08 22:35:37 +0900

    전역헤더설정

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import Layout from '../components/layout/layout'
 function MyApp({ Component, pageProps }) {
-  return <Layout><Component {...pageProps} /></Layout>
+  return <Layout><Head><meta name='desc' content='globalHead'/></Head><Component {...pageProps} /></Layout>
   }
 
 export default MyApp
