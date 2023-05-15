77fe8f84254e4157171a662278b94842e6d9ef5e
gkgjswls <gkgjswls842@gmail.com>
2023-05-15 16:41:31 +0900

    MongoDB연동

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,14 +1,18 @@
+import { MongoClient } from "mongodb";
 
 
-
-export default function handler(req, res) {
+export default async function handler(req, res) {
 
   if(req.method ==='POST'){
     const userEmail = req.body.email;
     if(!userEmail || !userEmail.includes('@')){
       return res.status(422).json({message: 'Invalid email address'})
     }
-    return res.status(201).json({news: userEmail})
+    const client = await MongoClient.connect('mongodb+srv://gkgjswls842:1234@cluster0.ht9pwmc.mongodb.net/newsletter?retryWrites=true&w=majority')
+    const db  = client.db();
+    return db.collection('emails').insertOne({email: userEmail})}
+    client.close();
+    res.status(201).json({news: userEmail})
   }
 
-}
+
