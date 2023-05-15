5447a1809ee67b25e43af843bb2a276ff52acb63
gkgjswls <gkgjswls842@gmail.com>
2023-05-15 18:16:43 +0900

    에러핸들링

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,18 +1,17 @@
-import { MongoClient } from "mongodb";
+import { connectDatabase, insertDocument,getAllDocuments } from "../../../helpers/db-util";
 
 
 export default async function handler(req, res) {
   const eventId = req.query.eventId;
-  const client = await MongoClient.connect('mongodb+srv://gkgjswls842:1234@cluster0.ht9pwmc.mongodb.net/events?retryWrites=true&w=majority')
-
+  let client;
   if(req.method ==='POST'){
     //추가
     const {email, name, text} = req.body;
     if(!email.includes('@') ||
-        !name||
-        name.trim() ===''||
-        !text ||
-        text.trim() === ''
+    !name||
+    name.trim() ===''||
+    !text ||
+    text.trim() === ''
     ) {
       return res.status(422).json({message: 'Invalid input'});
       
@@ -24,18 +23,43 @@ export default async function handler(req, res) {
       eventId
     }
     
-    const db  = client.db();
-    const result= await db.collection('comments').insertOne({newComment})
-    console.log(result)
-;   newComment.id = result.insertedId;
+    try{
+      client = await connectDatabase();
+    }
+    catch(err){
+      res.status(500).json({message: 'connecting to the database failed!'})
+      return ;
+    }
+    let result;
+    try{
+      result = await insertDocument(client,'comments',{newComment})
+      newComment._id = result.insertedId
+  
+      res.status(201).json({message: 'Added comment', comment: newComment})
+    }
+    catch(err){
+      res.status(500).json({message: 'Inserting data failed!'})
 
-    res.status(201).json({message: 'Added comment', comment: newComment})
+    }
   }
   if(req.method === 'GET'){
     //받기
-    const db = client.db();
-    const document = await db.collection('comments').find().sort({_id: -1}).toArray()
-
+    let client;
+    try{
+      client =await connectDatabase()
+    }
+    catch(err){
+      res.status(500).json({message: 'connecting to the database failed!'})
+      return ;
+    }
+    let document;
+    try{
+      document = await getAllDocuments(client,'comments',{_id: -1})
+    }
+    catch(err){
+      res.status(500).json({message: 'Data fetching failed'})
+      return;
+    }
 
     return res.status(201).json({data: document})
 
